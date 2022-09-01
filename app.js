db3f1f04c4e0a82d887ab099bd07fe80c4c46dfd
Reina Rodriguez <rrodriguez.ut@gmail.com>
2022-08-31 19:49:31 -0700

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+var person = {
+    name: "reina",
+    age: 26,
+    hobbies: ["hiking", "skiing", "reading", "chess"]
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
---
+++ app.ts
@@ -0,0 +1,13 @@
+const person = {
+
+    name: "reina",
+    age: 26,
+    hobbies: ["hiking", "skiing", "reading", "chess"]
+
+}
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby);
+}
