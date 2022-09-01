93ed755a8da47b6ae5c84d015d97edf9df5179c7
Reina Rodriguez <rrodriguez.ut@gmail.com>
2022-08-31 20:25:36 -0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 0] = "admin";
+    Role[Role["read_only"] = 1] = "read_only";
+    Role[Role["author"] = 2] = "author";
+})(Role || (Role = {}));
 var person = {
     name: "reina",
     age: 26,
-    hobbies: ["hiking", "skiing", "reading", "chess"]
+    hobbies: ["hiking", "skiing", "reading", "chess"],
+    role: Role.admin
 };
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.admin) {
+    console.log("is admin");
 }
--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
+enum Role {admin, read_only, author}
+
 const person = {
 
     name: "reina",
     age: 26,
-    hobbies: ["hiking", "skiing", "reading", "chess"]
+    hobbies: ["hiking", "skiing", "reading", "chess"],
+    role: Role.admin
 
 }
 
@@ -10,4 +13,8 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
     console.log(hobby);
+}
+
+if (person.role === Role.admin) {
+    console.log("is admin")
 }
